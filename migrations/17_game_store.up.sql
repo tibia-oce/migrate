94e92a9b419f86487566f71ebe1e03f27f41e86a
Jordan Hoare <jordanhoare0@gmail.com>
2024-10-13 11:45:16 +0800

    feat: use latest shop module schema

--- migrations/17_game_store.up.sql
+++ migrations/17_game_store.up.sql
@@ -1,9 +1,18 @@
-CREATE TABLE shop_history (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  account INT NOT NULL,
-  player INT NOT NULL,
-  date DATETIME NOT NULL,
-  title VARCHAR(100) NOT NULL,
-  cost INT NOT NULL,
-  details VARCHAR(500) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+CREATE TABLE `shop_history` (
+  `id` INT AUTO_INCREMENT PRIMARY KEY,
+  `account_id` INT NOT NULL,
+  `player_id` INT NOT NULL,
+  `purchase_date` DATETIME NOT NULL,
+  `title` VARCHAR(100) NOT NULL,
+  `price` INT NOT NULL,
+  `quantity` INT(11) NOT NULL DEFAULT 0,
+  `target` VARCHAR(255) DEFAULT NULL,
+  CONSTRAINT `fk_shop_history_account`
+    FOREIGN KEY (`account_id`) REFERENCES `accounts` (`id`)
+    ON DELETE CASCADE,
+  CONSTRAINT `fk_shop_history_player`
+    FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
+    ON DELETE CASCADE
+)
+ENGINE = InnoDB DEFAULT CHARACTER
+SET = utf8;
