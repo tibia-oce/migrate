c06fa18fbefae8627a683dcec27d26f05bc0d251
Jordan Hoare <jordan.hoare@outlook.com>
2024-10-28 17:49:55 +0800

    chore: clean up unused myaac links

--- migrations/7_myaac_menu.up.sql
+++ migrations/7_myaac_menu.up.sql
@@ -1,14 +1,6 @@
-/* MENU_CATEGORY_NEWS tibiacom */
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Latest News', 'news', 1, 0);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'News Archive', 'news/archive', 1, 1);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Changelog', 'changelog', 1, 2);
-/* MENU_CATEGORY_ACCOUNT tibiacom */
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Account Management', 'account/manage', 2, 0);
@@ -17,17 +9,10 @@ INSERT INTO `myaac_menu` (
 ) VALUES ('tibiacom', 'Create Account', 'account/create', 2, 1);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Lost Account?', 'account/lost', 2, 2);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Server Rules', 'rules', 2, 3);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Downloads', 'downloads', 2, 4);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Report Bug', 'bugtracker', 2, 5);
-/* MENU_CATEGORY_COMMUNITY tibiacom */
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Characters', 'characters', 3, 0);
@@ -45,43 +30,19 @@ INSERT INTO `myaac_menu` (
 ) VALUES ('tibiacom', 'Houses', 'houses', 3, 4);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Guilds', 'guilds', 3, 5);
+) VALUES ('tibiacom', 'Forum', 'forum', 3, 5);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Polls', 'polls', 3, 6);
+) VALUES ('tibiacom', 'Guilds', 'guilds', 3, 6);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Bans', 'bans', 3, 7);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Support List', 'team', 3, 8);
-/* MENU_CATEGORY_FORUM tibiacom */
+) VALUES ('tibiacom', 'Server Info', 'serverInfo', 5, 1);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Forum', 'forum', 4, 0);
-/* MENU_CATEGORY_LIBRARY tibiacom */
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Creatures', 'creatures', 5, 0);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Spells', 'spells', 5, 1);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Commands', 'commands', 5, 2);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Exp Stages', 'experienceStages', 5, 3);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Gallery', 'gallery', 5, 4);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Server Info', 'serverInfo', 5, 5);
-INSERT INTO `myaac_menu` (
-    `template`, `name`, `link`, `category`, `ordering`
-) VALUES ('tibiacom', 'Experience Table', 'experienceTable', 5, 6);
-/* MENU_CATEGORY_SHOP tibiacom */
+) VALUES ('tibiacom', 'Creatures', 'creatures', 5, 2);
 INSERT INTO `myaac_menu` (
     `template`, `name`, `link`, `category`, `ordering`
 ) VALUES ('tibiacom', 'Buy Points', 'points', 6, 0);
